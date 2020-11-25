6978024988c73cc550788678f0b3c2c4c0ee4a43
zipotron <zipotron@localhost.localdomain>
2020-11-25 12:45:29 +0200

    A bit cleaned signal

--- transmitter/src/main.c
+++ transmitter/src/main.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-	uint16_t c=0, min=4095, max=0, avr=0, count=0;
-	//uint16_t samples[4096];
-	//uint64_t aux;
-	uint16_t analog_read = 0;
+	uint16_t c=0, min=4095, max=0, avr=4020, count=0, resistence=5;
+	uint16_t samples[4096];
+	uint32_t aux;
+	//uint16_t analog_read = 0;
 	
 	lcd_init();
     led_init();
@@ -19,9 +19,13 @@ int main(void)
     LCD_Clear(BLACK);
     
     for(;;){
-		analog_read = ADC_RDATA(ADC0);
+		samples[count] = ADC_RDATA(ADC0);
 		
-		c = analog_read > 4020? 1 : 0;
+		if(c)
+			c = samples[count] > avr - resistence? 1 : 0;
+		else
+			c = samples[count] > avr + resistence? 1 : 0;
+		//c = samples[count] > min + ((max - min) / 2)? 1 : 0;
 		//c= c? 0:1;//For check sample rate in the osciloscope
 		
 		led_set(c);
@@ -29,30 +33,30 @@ int main(void)
 		
 		delay_1ms(1);
 		
-		/*if(analog_read < min)
-			min = analog_read;
+		if(samples[count] < min)
+			min = samples[count];
 		
-		if(analog_read > max)
-			max = analog_read;
+		if(samples[count] > max)
+			max = samples[count];
 		
-		count++;
-		
-		if(count == 65535){*/
-		/*if(count == 4096){
+		if(count == 4095){
 			aux = 0;
 			for(count=0;count<4096;count++)
-				aux=aux+((uint64_t)samples[count]);
-			avr=(uint16_t)(aux/4096);*/
-			/*count=0;
+				aux=aux+((uint32_t)samples[count]);
+			avr=(uint16_t)(aux/4096);
+			count=0;
 			
 			LCD_ShowString8(0, (1 << 3), (u8 *)" Lectura: ", YELLOW);//Just debugging
-			LCD_ShowNum(90, (1 << 3), (u16)analog_read, 6,YELLOW);//Debugging
+			LCD_ShowNum(90, (1 << 3), (u16)samples[count], 6,YELLOW);//Debugging
 			LCD_ShowString8(0, (4 << 3), (u8 *)" Minimo: ", GREEN);//Just debugging
 			LCD_ShowNum(90, (4 << 3), (u16)min, 6,GREEN);//Debugging
 			LCD_ShowString8(0, (6 << 3), (u8 *)" Maximo: ", RED);//Just debugging
-			LCD_ShowNum(90, (6 << 3), (u16)max, 6,RED);//Debugging*/
-			/*LCD_ShowString8(0, (8 << 3), (u8 *)" Media: ", YELLOW);//Just debugging
-			LCD_ShowNum(90, (8 << 3), (u16)avr, 6,YELLOW);//Debugging*/
-		//}
+			LCD_ShowNum(90, (6 << 3), (u16)max, 6,RED);//Debugging
+			LCD_ShowString8(0, (8 << 3), (u8 *)" Media: ", YELLOW);//Just debugging
+			LCD_ShowNum(90, (8 << 3), (u16)avr, 6,YELLOW);//Debugging
+			max=0;
+			min=4095;
+		} else
+			count++;
 	}
 }
